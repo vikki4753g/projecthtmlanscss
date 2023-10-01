a202daf98ba88e0f655ea86a1cd97bee5c0a505a
Vikas Patidar <116332152+vikki4753g@users.noreply.github.com>
2023-10-01 10:50:22 +0530

    Index.html cards.html and sttles.css uploaded

---
+++ javascript.js
@@ -0,0 +1,11 @@
+// ducument.firstElementChild.lastElementChild.innerHTML='vikas'
+// .click() method
+// ducument.getElementByTagName("li");
+// ducument.getElementById("li");
+// Document.querySelector()
+// Document.querySelector("h1").classList
+// Document.querySelector("h1").classList.add("invisible")
+// Document.querySelector("h1").classList.remove("invisible")
+// Document.querySelector("h1").classList.toggle("invisible")  if class aplied then it remove or if not applid then it apply
+// ducument.getElementByTagName("li").innerHTML="vikas";
+// ducument.getElementByTagName("li").textContent="vikas"; both same work
